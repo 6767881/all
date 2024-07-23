50273288deff5832a519ecfec2053510a558f61f
6767881 <89791061+6767881@users.noreply.github.com>
2024-07-24 00:49:40 +0800

    Update SetRootLoginOnLinux.sh

--- SetRootLoginOnLinux.sh
+++ SetRootLoginOnLinux.sh
@@ -1,1 +1,5 @@
-
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i '/Include/s/^/#/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+echo "root:mylsix0.0" | sudo chpasswd
