976bc1cadac1c970e5be26424423f2c031684b63
6767881 <89791061+6767881@users.noreply.github.com>
2024-07-24 23:03:04 +0800

    Create InstallX-ui.sh

---
+++ InstallX-ui.sh
@@ -0,0 +1,11 @@
+cd /root/
+wget https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz
+rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
